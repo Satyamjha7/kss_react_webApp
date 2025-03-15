bf971ac3116566267c61de33928e0d018812bdd2
Satyam Jha <satyam.k870z@gmail.com>
2025-03-16 00:37:17 +0530

    Update kss content

--- src/Page/StoryTellingVideos.jsx
+++ src/Page/StoryTellingVideos.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './StoryTellingVideos.css';
+import "./StorytellingVideos.css";
 import playButton from "../assets/play-button.png";
 
 import thumbnailImg from "../assets/133.jpeg";
