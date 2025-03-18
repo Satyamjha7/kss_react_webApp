ff67bdd99245eede5d335fd4ce68a88fad616f79
Satyam Jha <satyam.k870z@gmail.com>
2025-03-18 23:59:07 +0530

    Update kss content

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -73,8 +73,14 @@ export default function Footer() {
             </section>
 
             <div className="footer-bottom">
-                © 2025 Copyright:
-                <a href="https://mdbootstrap.com/" className="footer-brand">MDBootstrap.com</a>
+                <div className="footerlast">
+                    <p>© 2025 Kosi Seva Sadan. All rights reserved</p>
+                    <div className="footer-links">
+                        <a href="/copyright">Copyright</a>
+                        <a href="/privacy-policy">Privacy Policy and Cookie Notice</a>
+                        <a href="/terms-conditions">Terms and Conditions</a>
+                    </div>
+                </div>
             </div>
         </MDBFooter>
     );
